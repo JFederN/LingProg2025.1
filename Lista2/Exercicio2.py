4c3519b728f0c8f38bc31b5fa5438b88135609c2
JFederN <federgames@gmail.com>
2025-02-21 19:57:53 -0300

    Create Exercicio2.py

---
+++ Lista2/Exercicio2.py
@@ -0,0 +1,8 @@
+num1 = float(input("Digite o numero 1: "))
+num2 = float(input("Digite o numero 2: "))
+if num1 > num2:
+    print(num1,"é maior")
+if num2 > num1:
+    print(num2,"é maior")
+if num1 == num2:
+    print("Os numeros são iguais")
