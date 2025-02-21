3e09b4d717a3e3ba9adcc92725e0edc68651ba1b
JFederN <federgames@gmail.com>
2025-02-21 19:49:22 -0300

    Delete Exercicio5.py

--- Exercicio5.py
+++
@@ -1,7 +0,0 @@
-"""Desenvolva um programa que receba um valor em metros e o converta para centímetros.
-Sabemos que: 1 metro = 100 centimetros"""
-
-import math
-metro = float(input("Digite um valor em metros: "))
-centimetro = metro * 100
-print(centimetro,"centimetros")
