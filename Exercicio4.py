7a252255859d0293785b22b3ffe50a5b7d97ac0e
JFederN <federgames@gmail.com>
2025-02-21 19:49:15 -0300

    Delete Exercicio4.py

--- Exercicio4.py
+++
@@ -1,8 +0,0 @@
-"""Faça um programa que pergunte o número de horas trabalhadas no mês e o valor recebido
-por hora. O programa deve calcular e exibir o salário total do mês."""
-
-import math
-horas = float(input("Digite o numero de horas trabalhadas no mes: "))
-salHora = float(input("Digite o valor da hora de trabalho: "))
-SalarioTotal = horas * salHora
-print(SalarioTotal)
