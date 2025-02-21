45bc7cd069d65c2901d714b7204c6baaecabb441
JFederN <federgames@gmail.com>
2025-02-21 20:24:57 -0300

    Create Exercicio3.py

---
+++ Lista2/Exercicio3.py
@@ -0,0 +1,9 @@
+import math
+
+idade = float(input("Digite sua idade: "))
+if idade >= 18 and idade <= 59:
+    print("adulto")
+if idade <18:
+    print("menor de idade")
+if idade> 60:
+    print("idoso")
